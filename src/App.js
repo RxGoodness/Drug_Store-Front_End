a184fed8c5ab081f528aca11aec3eb133ef3e0a2
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-13 20:37:45 +0100

    Redux initiation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
+import store from './redux/Store';
+import { Provider } from 'react-redux';
 import './App.css';
 import DrugContainer from './component/DrugContainer';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <DrugContainer/>
     </div>
+    </Provider>
   );
 }
 
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,1 +1,6 @@
 import { createStore } from 'redux';
+import DrugReducer from '../Drugs/DrugReducer';
+
+const store = createStore(DrugReducer)
+
+export default store 
